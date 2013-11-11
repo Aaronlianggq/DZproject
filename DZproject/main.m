2c542ac54e7bb0d95e22590e2908c8b9d4911ce5
Aaronlianggq <897135405@qq.com>
2013-11-11 11:05:43 +0800

    Initial Commit

---
+++ DZproject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DZproject
+//
+//  Created by lianggq on 13-11-11.
+//  Copyright (c) 2013年 lianggq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LGQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LGQAppDelegate class]));
+    }
+}
---
+++ DZprojectTests/DZprojectTests.m
@@ -0,0 +1,34 @@
+//
+//  DZprojectTests.m
+//  DZprojectTests
+//
+//  Created by lianggq on 13-11-11.
+//  Copyright (c) 2013年 lianggq. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DZprojectTests : XCTestCase
+
+@end
+
+@implementation DZprojectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
